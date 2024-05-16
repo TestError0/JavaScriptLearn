f60edca6be1f1f0de895ca57145f74c182741da0
TestError0 <ashraf.inflack@gmail.com>
2024-05-16 19:43:23 +0600

    Addition code added

--- First/function.js
+++ First/function.js
@@ -1,6 +1,19 @@
-function myFunction(fullName){
-    console.log('Hello ' + fullName);
+
+// function myFunction(fullName){
+//     console.log('Hello ' + fullName);
+// }
+
+// myFunction('Kazi Shiraz');
+// myFunction('Sir');
+
+
+
+
+function Addition(a,b){
+    return a + b;
 }
 
-myFunction('Kazi Shiraz');
-myFunction('Sir');
+let a = 5;
+let b = 10;
+let sum = Addition(a,b);
+console.log("Sum is :", sum);

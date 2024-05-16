04a73dbd462cd1dba48f79e8167439211f0f41bf
TestError0 <ashraf.inflack@gmail.com>
2024-05-16 18:45:23 +0600

    New File added

---
+++ First/app.js
@@ -0,0 +1,243 @@
+/*
+ // Ternary Operator (condition operator er short cut)
+
+
+let a = 10;
+let b = 8;
+
+let t = a < b ? 'True' : 'False';
+
+console.log(t);
+
+*/
+
+
+
+/*
+   //Assignment Operator
+
+let j = 10;
+let k = 20;
+
+j = j +k;
+j += k;
+ 
+j = j - k;
+j -= k;
+
+console.log(j);
+
+*/
+
+
+
+
+
+
+
+/*
+
+//Logical Operater
+
+// AND(&&) = 2ta e true hote hobe
+// OR(||) = 2ta false holei false otherwise sob true
+// NOT (!) = not use kore not value banano
+
+
+*/
+
+
+
+
+
+ 
+/*
+//Comparison Operator
+
+let p = '5';
+let q = 4;
+
+console.log(p == q); // equal to 
+console.log(p === q); // equal value and equal type
+console.log(p != q); //not equal
+console.log(p > q);  // Grater Then
+console.log(p < q);  //Less then 
+console.log(p > q);  //Less then Equal
+console.log(p >= q);  //Less then or Equal to
+console.log(p <= q); //Greater Then Equal to
+
+*/
+
+
+
+
+
+
+//Increment & Decrement 
+
+/*
+
+let x = 5;
+x = x + 1;
+x++;
+console.log(`Increment ${x}`);
+
+
+let y = 5;
+y = y - 1;
+y--; 
+console.log(`Decrement ${y}`);
+
+*/
+
+
+
+
+
+
+/*
+
+//Arithmetic Operators
+
+let userInput = prompt('Enter a number');
+let number = parseInt(userInput);
+
+if(number %2 === 0){
+    console.log(`${number} is Even Number`);
+}
+else{
+    console.log(`${number} is Odd Number`);
+}
+
+
+// if(number % 2 ===0){
+//     console.log`This is Odd Number`;
+// }
+
+// else{
+//     console.log`This is Even Number`;
+// }
+
+
+
+
+// let a = 10;
+// let b = 5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+*/
+
+
+
+
+
+
+
+/*
+// ?JavaScript Variables
+
+
+const c = 10;
+const e = 20;
+
+{
+    const d =50;
+    console.log(c);
+    console.log(e);
+}
+
+*/
+
+
+
+
+/*
+
+//?LET  variales
+
+let b =10;
+b = 11;
+
+{
+
+console.log(b);
+
+}
+
+
+*/
+
+
+
+
+
+/*
+?var variable
+*Can re declare
+*can re assign
+*Global
+*/
+
+
+/*
+var a = 10;
+var a =20;
+a ='Dhaka';
+{
+    var blocked = '30';
+    console.log(a);
+}
+console.log(blocked);
+*/
+
+
+
+
+
+
+/*
+let fruits = 'Mango';
+console.log(`${fruits} is my fav`);
+*/
+
+
+
+
+//?Comment Section
+
+/*
+**Multiline comment
+? some info
+TODO: something I need
+function funcName(){
+    console.log('My First Function');
+
+}
+funcName();
+
+*/
+
+
+
+
+
+//Starting code
+
+/*
+console.log('Hello JavaScript');
+
+let sum = 50 + 60;
+console.log(sum)
+
+let firstname = 'Kazi';
+let age = '20';
+
+console.log(firstname);
+console.log(age);
+
+*/
